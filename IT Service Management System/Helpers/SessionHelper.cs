0fad0a43b577620e199d4f9b3d045e92792f5696
AnoldKudakwasheKupara <kuparaanold20@gmail.com>
2026-03-19 16:05:05 +0200

    added SessionHelper class

---
+++ IT Service Management System/Helpers/SessionHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace IT_Service_Management_System.Helpers
+{
+    public static class SessionHelper
+    {
+        public static bool IsAdmin(HttpContext context)
+        {
+            return context.Session.GetString("UserRole") == "Admin";
+        }
+
+        public static bool IsLoggedIn(HttpContext context)
+        {
+            return context.Session.GetInt32("UserId") != null;
+        }
+    }
+}
