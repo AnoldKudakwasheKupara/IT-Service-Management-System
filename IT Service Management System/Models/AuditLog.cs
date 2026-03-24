99149e0b25d17fc99ccd6ec71c27e93420eabe67
AnoldKudakwasheKupara <kuparaanold20@gmail.com>
2026-03-24 16:07:29 +0200

    updated the AuditLog model tp include location and device type

--- IT Service Management System/Models/AuditLog.cs
+++ IT Service Management System/Models/AuditLog.cs
@@ -4,17 +4,19 @@
     {
         public int Id { get; set; }
 
-        public string UserId { get; set; } // Who did it
+        public string UserId { get; set; }
         public string UserName { get; set; }
 
-        public string Action { get; set; } // e.g. "Created Ticket"
-        public string Entity { get; set; } // e.g. "Ticket"
-        public int? EntityId { get; set; } // Ticket ID
+        public string Action { get; set; } 
+        public string Entity { get; set; } 
+        public int? EntityId { get; set; }
 
-        public string Details { get; set; } // Optional description
+        public string Details { get; set; } 
 
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 
         public string IpAddress { get; set; }
+        public string Location { get; set; } 
+        public string Device { get; set; }  
     }
 }
