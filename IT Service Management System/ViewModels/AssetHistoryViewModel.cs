6f58ca205bb2f16eeb42b99f53d3fc8dbb2a7b4d
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-03-26 01:13:08 +0200

    adde dthe AssetHistoryViewModel

---
+++ IT Service Management System/ViewModels/AssetHistoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using IT_Service_Management_System.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace IT_Service_Management_System.ViewModels
+{
+    public class AssetHistoryViewModel
+    {
+        public int AssetId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string EventType { get; set; }
+        public string Condition { get; set; }
+
+        public string PerformedBy { get; set; }
+        public string Remarks { get; set; }
+
+        [ValidateNever]
+        public List<User> Users { get; set; }
+    }
+}
