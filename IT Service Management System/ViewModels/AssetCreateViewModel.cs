388acc7b85c83674f368947c31b60326eb7f46e9
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-03-26 00:08:24 +0200

    added AssetCreateViewModel

---
+++ IT Service Management System/ViewModels/AssetCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using IT_Service_Management_System.Models;
+
+namespace IT_Service_Management_System.ViewModels
+{
+    public class AssetCreateViewModel
+    {
+        public Asset Asset { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
