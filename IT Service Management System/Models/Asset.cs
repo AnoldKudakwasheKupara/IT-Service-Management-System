a0c57de1175790aaa04519bd27e8b0eda425006c
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-03-25 23:53:30 +0200

    added asset model

---
+++ IT Service Management System/Models/Asset.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace IT_Service_Management_System.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int? UserId { get; set; }
+        [ValidateNever]
+        public User User { get; set; }
+
+        public string ItemName { get; set; }
+        public string SerialNumber { get; set; }
+
+        public string ActionType { get; set; }
+        public string Condition { get; set; }
+
+        public string IssuedBy { get; set; }
+        public string Remarks { get; set; }
+    }
+}
