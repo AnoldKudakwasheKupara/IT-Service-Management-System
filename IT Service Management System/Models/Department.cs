d0686b021c33d76627d685f2b0dd419a90b91b35
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-09 15:22:26 +0200

    added Department model

---
+++ IT Service Management System/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace IT_Service_Management_System.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
