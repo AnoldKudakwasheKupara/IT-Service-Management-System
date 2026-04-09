a1bc21eefde7ce6d64444164c2e56aba573f09d6
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-09 16:26:03 +0200

    Added RenewalFields

---
+++ IT Service Management System/Migrations/20260409142528_AddRenewalFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IT_Service_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRenewalFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRenewed",
+                table: "SSLCertificates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRenewedDate",
+                table: "SSLCertificates",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRenewed",
+                table: "SSLCertificates");
+
+            migrationBuilder.DropColumn(
+                name: "LastRenewedDate",
+                table: "SSLCertificates");
+        }
+    }
+}
