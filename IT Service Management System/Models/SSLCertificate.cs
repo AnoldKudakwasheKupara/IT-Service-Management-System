30f8e6e936bbeda605100ba3dc79d707831766f4
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-09 16:25:11 +0200

    updated the model to show is renewed option

--- IT Service Management System/Models/SSLCertificate.cs
+++ IT Service Management System/Models/SSLCertificate.cs
@@ -5,10 +5,13 @@
         public int Id { get; set; }
 
         public string SystemName { get; set; }
-
         public string URL { get; set; }
 
         public DateTime ExpiryDate { get; set; }
+
+        public bool IsRenewed { get; set; } = false;
+
+        public DateTime? LastRenewedDate { get; set; }
 
         public int DaysRemaining => (ExpiryDate - DateTime.Now).Days;
     }
