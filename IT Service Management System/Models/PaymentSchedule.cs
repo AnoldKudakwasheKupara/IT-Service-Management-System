edd12aa728dd472caac703f8f5203de6a0c41b5a
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-10 15:37:46 +0200

    added Payment Schedule model

---
+++ IT Service Management System/Models/PaymentSchedule.cs
@@ -0,0 +1,29 @@
+﻿namespace IT_Service_Management_System.Models
+{
+    public class PaymentSchedule
+    {
+        public int Id { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int? DayOfMonth { get; set; }
+
+        public DateTime? FixedDate { get; set; }
+
+        public PaymentFrequency Frequency { get; set; }
+
+        public string Departments { get; set; }
+
+        public DateTime NextRunDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
+
+public enum PaymentFrequency
+{
+    Monthly,
+    Annual
+}
