155badd649050a113175e6ad11aa35b1bc79dd71
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-12 12:03:01 +0200

    added migrations to update payments schedules table

---
+++ IT Service Management System/Migrations/20260412100213_Update.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IT_Service_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayOfMonth",
+                table: "PaymentSchedules");
+
+            migrationBuilder.DropColumn(
+                name: "FixedDate",
+                table: "PaymentSchedules");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentDate",
+                table: "PaymentSchedules",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDate",
+                table: "PaymentSchedules");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DayOfMonth",
+                table: "PaymentSchedules",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FixedDate",
+                table: "PaymentSchedules",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
