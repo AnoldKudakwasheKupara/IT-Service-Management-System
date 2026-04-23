822f097351cab9f50176edf5b999b06c29cd8d67
Anold Kupara <Anold Kupara@DESKTOP-5RAH3M0>
2026-04-23 12:50:06 +0200

    added Activity model

---
+++ IT Service Management System/Models/Activity.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IT_Service_Management_System.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } 
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        [NotMapped]
+        public TimeSpan? Duration
+        {
+            get
+            {
+                if (EndTime.HasValue)
+                    return EndTime.Value - StartTime;
+                return null;
+            }
+        }
+
+        public int CategoryId { get; set; }
+        public ActivityCategory Category { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
