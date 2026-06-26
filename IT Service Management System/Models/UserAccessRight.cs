890c6067e4b70fc3005dcb82296728826b37c62c
Anold Kupara <Anold Kupara@DESKTOP-F1L0SD7>
2026-06-26 09:33:41 +0200

    Add UserAccessRight and UserAccessRightItem models
    Introduced UserAccessRight and UserAccessRightItem classes to represent user access rights records and their associated user permissions, including solution details, assignment, review, approval metadata, and various access flags.

---
+++ IT Service Management System/Models/UserAccessRight.cs
@@ -0,0 +1,39 @@
+﻿namespace IT_Service_Management_System.Models
+{
+    public class UserAccessRight
+    {
+        public int Id { get; set; }
+
+        public string? SolutionSoftware { get; set; }
+
+        public string? ProductionServerId { get; set; }
+
+        public string? ClientName { get; set; }
+
+        public string? VersionReleaseNo { get; set; }
+
+        public string? SystemsAdmin { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public List<UserAccessRightItem> Users { get; set; } = new();
+
+        public string? AssignedBy { get; set; }
+
+        public string? AssignedSignature { get; set; }
+
+        public DateTime? AssignedDate { get; set; }
+
+        public string? ReviewedBy { get; set; }
+
+        public string? ReviewedSignature { get; set; }
+
+        public DateTime? ReviewedDate { get; set; }
+
+        public string? ApprovedBy { get; set; }
+
+        public string? ApprovedSignature { get; set; }
+
+        public DateTime? ApprovedDate { get; set; }
+    }
+}
---
+++ IT Service Management System/Models/UserAccessRightItem.cs
@@ -0,0 +1,25 @@
+﻿namespace IT_Service_Management_System.Models
+{
+    public class UserAccessRightItem
+    {
+        public int Id { get; set; }
+
+        public int UserAccessRightsId { get; set; }
+
+        public UserAccessRight UserAccessRight { get; set; }
+
+        public string? UserName { get; set; }
+
+        public bool UserManagement { get; set; }
+
+        public bool Initiate { get; set; }
+
+        public bool Confirmation { get; set; }
+
+        public bool Approval { get; set; }
+
+        public bool AccountManagement { get; set; }
+
+        public bool Reports { get; set; }
+    }
+}
